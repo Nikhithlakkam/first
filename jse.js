d3d987f11bcc0eb854686040c403cc23217a7a0d
Nikhithlakkam <144144719+Nikhithlakkam@users.noreply.github.com>
2023-09-05 20:26:02 +0530

    Add files via upload

---
+++ jse.js
@@ -0,0 +1,10 @@
+function Wikipedia() {
+    var linkUrl = document.getElementById("link").value;
+    var link = "https://en.wikipedia.org/wiki/" + linkUrl;
+    window.open(link, '_blank');
+}
+function spotify() {
+    var linkUrl = document.getElementById("lin").value;
+    var link = "https://open.spotify.com/search/" + linkUrl;
+    window.open(link, '_blank');
+}
